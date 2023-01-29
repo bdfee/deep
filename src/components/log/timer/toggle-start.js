805dfebc82f97d7e6a82b2227a5929f27f4ec047
Ben Durfee <ben.p.durfee@gmail.com>
2023-01-29 16:21:27 -0500

    removed toggle component, start and stop buttons, and refactored setting the count up time

--- src/components/log/timer/toggle-start.js
+++
@@ -1,9 +0,0 @@
-const ToggleStart = ({ isActive, handleStartTimer, handleStopTimer }) => {
-  return !isActive ? (
-    <button onClick={handleStartTimer}>start</button>
-  ) : (
-    <button onClick={handleStopTimer}>pause</button>
-  )
-}
-
-export default ToggleStart
