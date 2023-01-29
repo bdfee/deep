b9333ebe3712fbd10471d2cfcdc1a3b2cb34fce8
Ben Durfee <ben.p.durfee@gmail.com>
2023-01-29 15:37:22 -0500

    container

--- src/components/container.js
+++ src/components/container.js
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Log from './log/index'
 import AudioParameters from './audio/audio-parameters'
 
+// container toggles between form and playing status
+
 const Container = () => {
   const [isRunning, setIsRunning] = useState(false)
   return (
